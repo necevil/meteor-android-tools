578c0c4319c082da72bdfa0d0b70fe9061cb6696
Scott Imbrie <scott@originalskateboards.com>
2016-10-18 17:49:45 -0400

    Adding accept licenses script.

---
+++ meteor-android-accept-licenses.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect -f
+
+#Dockerfile can be found in docker hub
+
+set timeout 1800
+set cmd [lindex $argv 0]
+set licenses [lindex $argv 1]
+
+spawn {*}$cmd
+expect {
+  "Do you accept the license '*'*" {
+        exp_send "y\r"
+        exp_continue
+  }
+  eof
+}
